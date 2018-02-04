c3cc70542b5100807e8551a90df19d2919fa7af7
Richard Macdonald <richard@thewidgetsmith.com>
2018-02-03 21:05:13 -0700

    stubs out custom bar chart to the satellites view

--- app/src/main/java/org/publicntp/gnssreader/ui/SatelliteFragment.java
+++ app/src/main/java/org/publicntp/gnssreader/ui/SatelliteFragment.java
@@ -16,38 +16,49 @@ import org.publicntp.gnssreader.ui.chart.SatelliteEntry;
 import org.publicntp.gnssreader.ui.chart.SatelliteMarkerView;
 import org.publicntp.gnssreader.ui.chart.SatellitePointValueFormatter;
 import org.publicntp.gnssreader.ui.chart.SatelliteRadialPlotChart;
+import org.publicntp.gnssreader.ui.chart.SatelliteSignalChart;
 
 
 import android.graphics.Color;
 
+import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.components.AxisBase;
 import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.components.MarkerView;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
 
 public class SatelliteFragment extends Fragment {
 
-    private SatelliteRadialPlotChart mChart;
+    private SatelliteRadialPlotChart mPositionChart;
+    private SatelliteSignalChart mSignalChart;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_satellite, container, false);
 
-        mChart = view.findViewById(R.id.satellite_radial_plot);
-        mChart.setBackgroundColor(Color.WHITE);
 
-        mChart.getDescription().setEnabled(false);
+////[ BEGIN Position Chart ]////
 
-        mChart.setWebLineWidth(1f);
-        mChart.setWebColor(Color.LTGRAY);
-        mChart.setWebLineWidthInner(1f);
-        mChart.setWebColorInner(Color.LTGRAY);
-        mChart.setWebAlpha(100);
+        mPositionChart = view.findViewById(R.id.satellite_position_radial_chart);
+        mPositionChart.setBackgroundColor(Color.WHITE);
+
+        mPositionChart.getDescription().setEnabled(false);
+
+        mPositionChart.setWebLineWidth(1f);
+        mPositionChart.setWebColor(Color.LTGRAY);
+        mPositionChart.setWebLineWidthInner(1f);
+        mPositionChart.setWebColorInner(Color.LTGRAY);
+        mPositionChart.setWebAlpha(100);
 
         // create a custom MarkerView (extend MarkerView) and specify the layout
         // to use for it
@@ -55,14 +66,14 @@ public class SatelliteFragment extends Fragment {
 //        mv.setChartView(mChart); // For bounds control
 //        mChart.setMarker(mv); // Set the marker to the chart
 
-        setData();
+        setPositionData();
 
 //        mChart.animateXY(
 //                1400, 1400,
 //                Easing.EasingOption.EaseInOutQuad,
 //                Easing.EasingOption.EaseInOutQuad);
 
-        XAxis xAxis = mChart.getXAxis();
+        XAxis xAxis = mPositionChart.getXAxis();
         xAxis.setDrawLabels(false);
 //        xAxis.setValueFormatter(new IAxisValueFormatter() {
 //
@@ -75,10 +86,10 @@ public class SatelliteFragment extends Fragment {
 //            }
 //        });
 
-        YAxis yAxis = mChart.getYAxis();
+        YAxis yAxis = mPositionChart.getYAxis();
         yAxis.setDrawLabels(false);
 
-        Legend l = mChart.getLegend();
+        Legend l = mPositionChart.getLegend();
         l.setEnabled(false);
 //        l.setVerticalAlignment(Legend.LegendVerticalAlignment.TOP);
 //        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.CENTER);
@@ -88,10 +99,76 @@ public class SatelliteFragment extends Fragment {
 //        l.setYEntrySpace(5f);
 //        l.setTextColor(Color.BLACK);
 
+////[ BEGIN Signal Quality Chart ]////
+
+        mSignalChart = view.findViewById(R.id.satellite_signal_bar_chart);
+//        mSignalChart.setOnChartValueSelectedListener(this);
+
+        mSignalChart.setDrawBarShadow(false);
+        mSignalChart.setDrawValueAboveBar(true);
+
+        mSignalChart.getDescription().setEnabled(false);
+
+        // if more than 60 entries are displayed in the chart, no values will be
+        // drawn
+        mSignalChart.setMaxVisibleValueCount(60);
+
+        // scaling can now only be done on x- and y-axis separately
+        mSignalChart.setPinchZoom(false);
+
+        mSignalChart.setDrawGridBackground(false);
+        // mChart.setDrawYLabels(false);
+
+        //IAxisValueFormatter xAxisFormatter = new DayAxisValueFormatter(mChart);
+
+        XAxis xAxisSig = mSignalChart.getXAxis();
+        xAxisSig.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxisSig.setDrawGridLines(false);
+        xAxisSig.setGranularity(1f); // only intervals of 1 day
+        xAxisSig.setLabelCount(7);
+        //xAxis.setValueFormatter(xAxisFormatter);
+
+        //IAxisValueFormatter custom = new MyAxisValueFormatter();
+
+        YAxis leftAxis = mSignalChart.getAxisLeft();
+        leftAxis.setLabelCount(8, false);
+        //leftAxis.setValueFormatter(custom);
+        leftAxis.setPosition(YAxis.YAxisLabelPosition.OUTSIDE_CHART);
+        leftAxis.setSpaceTop(15f);
+        leftAxis.setAxisMinimum(0f); // this replaces setStartAtZero(true)
+
+        YAxis rightAxis = mSignalChart.getAxisRight();
+        rightAxis.setDrawGridLines(false);
+        rightAxis.setLabelCount(8, false);
+        //rightAxis.setValueFormatter(custom);
+        rightAxis.setSpaceTop(15f);
+        rightAxis.setAxisMinimum(0f); // this replaces setStartAtZero(true)
+
+        Legend legend = mSignalChart.getLegend();
+        legend.setEnabled(false);
+//        legend.setVerticalAlignment(Legend.LegendVerticalAlignment.BOTTOM);
+//        legend.setHorizontalAlignment(Legend.LegendHorizontalAlignment.LEFT);
+//        legend.setOrientation(Legend.LegendOrientation.HORIZONTAL);
+//        legend.setDrawInside(false);
+//        legend.setForm(Legend.LegendForm.SQUARE);
+//        legend.setFormSize(9f);
+//        legend.setTextSize(11f);
+//        legend.setXEntrySpace(4f);
+        // l.setExtra(ColorTemplate.VORDIPLOM_COLORS, new String[] { "abc",
+        // "def", "ghj", "ikl", "mno" });
+        // l.setCustom(ColorTemplate.VORDIPLOM_COLORS, new String[] { "abc",
+        // "def", "ghj", "ikl", "mno" });
+
+//        XYMarkerView mv = new XYMarkerView(this, xAxisFormatter);
+//        mv.setChartView(mSignalChart); // For bounds control
+//        mSignalChart.setMarker(mv); // Set the marker to the chart
+
+        setSignalData(12, 50);
+
         return view;
     }
 
-    public void setData() {
+    public void setPositionData() {
 
         float min = 1;
         int count = 10;
@@ -122,7 +199,7 @@ public class SatelliteFragment extends Fragment {
         set.setDrawFilled(false);
         set.setDrawHighlightCircleEnabled(false);
         set.setDrawHighlightIndicators(false);
-        //set.setValueFormatter(new SatellitePointValueFormatter(0));
+        set.setValueFormatter(new SatellitePointValueFormatter(0));
 
         ArrayList<ISatelliteDataSet> sets = new ArrayList<>();
         sets.add(set);
@@ -132,9 +209,9 @@ public class SatelliteFragment extends Fragment {
         data.setDrawValues(true);
         data.setValueTextColor(Color.BLACK);
 
-        mChart.setRotationEnabled(false);
-        mChart.setData(data);
-        mChart.invalidate();
+        mPositionChart.setRotationEnabled(false);
+        mPositionChart.setData(data);
+        mPositionChart.invalidate();
     }
 
     private Drawable getEntryShape(boolean usedInFix, short signalQuality) {
@@ -153,6 +230,49 @@ public class SatelliteFragment extends Fragment {
         }
 
         return shape;
+    }
+
+    private void setSignalData(int count, float range) {
+
+        float start = 1f;
+
+        ArrayList<BarEntry> yVals1 = new ArrayList<BarEntry>();
+
+        for (int i = (int) start; i < start + count + 1; i++) {
+            float mult = (range + 1);
+            float val = (float) (Math.random() * mult);
+
+//            if (Math.random() * 100 < 25) {
+//                yVals1.add(new BarEntry(i, val, getResources().getDrawable(R.drawable.star)));
+//            } else {
+                yVals1.add(new BarEntry(i, val));
+//            }
+        }
+
+        BarDataSet set1;
+
+        if (mSignalChart.getData() != null &&
+                mSignalChart.getData().getDataSetCount() > 0) {
+            set1 = (BarDataSet) mSignalChart.getData().getDataSetByIndex(0);
+            set1.setValues(yVals1);
+            mSignalChart.getData().notifyDataChanged();
+            mSignalChart.notifyDataSetChanged();
+        } else {
+            set1 = new BarDataSet(yVals1, "The year 2017");
+
+            set1.setDrawIcons(false);
+
+            set1.setColors(ColorTemplate.MATERIAL_COLORS);
+
+            ArrayList<IBarDataSet> dataSets = new ArrayList<IBarDataSet>();
+            dataSets.add(set1);
+
+            BarData data = new BarData(dataSets);
+            data.setValueTextSize(10f);
+            data.setBarWidth(0.9f);
+
+            mSignalChart.setData(data);
+        }
     }
 
     public static SatelliteFragment newInstance() {
